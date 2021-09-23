dd12d074707dce0798ef858c1a29ccecdd3d8031
Valentina Colmenares <you@example.com>
2021-09-23 12:11:36 -0500

    Homework 6

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -4,17 +4,23 @@ function mayuscula(nombre) {
   //La función recibe un nombre y debe devolver el mismo que recibe pero con su primer letra en mayúscula
   //ej: Recibe "mario" ----> Devuelve "Mario"
   //Tu código:
+  function capitalize(word) {
+    return word[0].toUpperCase() + word.substring(1).toLowerCase();
+  }
+  return (capitalize(nombre))
 }
 
 function invocarCallback(cb) {
   // Invoca al callback `cb`
   //Tu código:
+  cb()
 }
 
 function operacionMatematica(n1, n2, cb) {
   //Vamos a recibir una función que realiza una operación matemática como callback junto con dos números.
   //Devolver el callback pasándole como argumentos los números recibidos.
   //Tu código:
+  return(cb(n1,n2))
 }
 
 function sumarArray(numeros, cb) {
@@ -22,12 +28,19 @@ function sumarArray(numeros, cb) {
   // Pasa el resultado a `cb`
   // No es necesario devolver nada
   //Tu código:
+  const suma = numeros.reduce(function(acc, elemento){
+    return acc + elemento;
+  })
+  cb(suma)
 }
 
 function forEach(array, cb) {
   // Itera sobre la matriz "array" y pasa los valores al callback uno por uno
   // Pista: Estarás invocando a `cb` varias veces (una por cada valor en la matriz)
   //Tu código:
+  array.forEach(function(elemento,indice){
+    cb(elemento)
+  })
 }
 
 function map(array, cb) {
@@ -35,12 +48,21 @@ function map(array, cb) {
   // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
   // El nuevo array debe tener la misma longitud que el array del argumento
   //Tu código:
+  var NwArray= array.map(function(elemento, indice){
+    return cb(elemento)
+  })
+  return(NwArray)
 }
 
 function filter(array) {
   //Filtrar todos los elementos del array que comiencen con la letra "a".
   //Devolver un nuevo array con los elementos que cumplen la condición
   //Tu código:
+  a=[]
+  array.forEach(function(elemento,indice){
+    if(elemento[0] == "a"){a.push(elemento)}
+  })
+  return(a)
 }
 
 // No modificar nada debajo de esta línea
